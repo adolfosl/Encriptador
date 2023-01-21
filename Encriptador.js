5f8ebe7b9bb789af962f79a326bf9c34a16a9e51
Gustavo <gustavoadsan@gmail.com>
2023-01-21 10:02:52 -0600

    Agregada la funcion de encriptar

--- Encriptador.js
+++ Encriptador.js
@@ -7,4 +7,23 @@ const mensaje = document.querySelector(".mensaje");
 //La letra "o" es convertida para "ober"
 //La letra "u" es convertida para "ufat"
 
-let matrizCodigo = [[e,enter],[i,imes][a,ai][o,ober][u,ufat]]
+function btnencriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value =""
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar (stringEncriptada){
+    let matrizCodigo = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for (let i= 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
